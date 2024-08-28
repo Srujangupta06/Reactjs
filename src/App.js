6406498c91df293e119640f3848fc88fe9d9df3b
Srujangupta06 <srujan93811@gmail.com>
2024-08-28 11:07:40 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Build UI with React Library
+        Build UI with React Library Or React Framework
         </p>
         <a
           className="App-link"
